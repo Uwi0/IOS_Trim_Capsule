5acbacd831ec6fed8f79eaf28387adc43a05d551
uwi0 <kakapoweb@gmail.com>
2024-08-29 03:31:54 +0700

    Initial Commit

---
+++ Trim Capsule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trim Capsule
+//
+//  Created by dwi prasetyo on 29/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trim Capsule/Trim_CapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trim_CapsuleApp.swift
+//  Trim Capsule
+//
+//  Created by dwi prasetyo on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Trim_CapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
